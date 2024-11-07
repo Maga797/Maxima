24e225614e2992165aeb1cbaebc3831a870d1901
Maga797 <magomeddalgatov44@gmail.com>
2024-11-08 00:55:14 +0300

    Array

---
+++ src/Array.java
@@ -0,0 +1,11 @@
+package src;
+
+public class Array {
+public static void main (String[] args) {
+    int [] array3 = {2, 4, 8, 16};
+
+    for (int i = 0; i < array3.length; i++) {
+        System.out.println(array3[i]);
+    }
+}
+}
